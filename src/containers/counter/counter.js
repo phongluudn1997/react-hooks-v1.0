208e9360146b08096ad123330c7ae78c3598f616
Luu Huynh Phong <luu.huynh@fiisoft.com>
2020-07-15 14:11:11 +0700

    #wip

--- src/containers/counter/counter.js
+++ src/containers/counter/counter.js
@@ -2,10 +2,15 @@ import React, { useState, useEffect } from "react";
 
 const Counter = () => {
   const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    document.title = `You clicked ${count} times.`;
+  });
+
   return (
     <div>
       <p>You clicked {count} times.</p>
-      <button onClick={() => setCount(count + 1)}></button>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
     </div>
   );
 };
